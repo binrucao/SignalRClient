b271d0a3ccc68f47fd29fa0ab01516f9e7a43a64
binrucao <binrucao@umass.edu>
2017-06-30 14:01:18 -0400

    Initial

---
+++ SignalRClient/ViewController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using MonoTouch.Dialog;
+using UIKit;
+
+namespace SignalRClient
+{
+	public partial class ViewController : DialogViewController
+	{
+
+		private readonly EntryElement _input;
+
+		private readonly Section _messages;
+
+		private readonly Client _client;
+
+
+		public ViewController() : base(UITableViewStyle.Grouped, null)
+		{
+
+			_input = new EntryElement(null, "Enter message", null)
+			{
+
+				ReturnKeyType = UIReturnKeyType.Send
+
+			};
+
+			_messages = new Section();
+
+
+			Root = new RootElement("Chat Client")
+			{
+				new Section { _input },
+				_messages
+			};
+			_client = new Client("iOS");
+		}
+
+		public override async void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			await _client.Connect();
+			_input.ShouldReturn += () =>
+			{
+				_input.ResignFirstResponder(true);
+
+				if (string.IsNullOrEmpty(_input.Value))
+					return true;
+					
+				_client.Send(_input.Value);
+
+				_input.Value = "";
+
+				return true;
+			};
+
+			_client.OnMessageReceived += (sender, message) =>
+				InvokeOnMainThread(() => _messages.Add(new StringElement(message)));
+
+
+		// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+		// Release any cached data, images, etc that aren't in us
+		}
+	}
+}
